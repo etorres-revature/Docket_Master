a9e6fe622c4665f9f634ff9c49e2cfdb1f80dad7
Eric D. Torres <etorresnotary@gmail.com>
2020-10-24 14:02:05 -0500

    git ignore too

---
+++ routes/api/case.js
@@ -0,0 +1,63 @@
+const { decodeBase64 } = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+const case = require("");
+
+
+//@route        GET / 
+//@desc         Get all cases
+//@access        Public
+router.get("/", (req, res) => {
+    db.Case.finAll({}).then( cases => {
+        res.json(cases)
+    })
+})
+
+//@route        GET /api/cases/:id 
+//@desc         Get case
+//@access       Public
+router.get("/api/cases/:id", (req, res) => {
+    db.Case.findOne({
+        where: {
+            id req.params.id
+        }
+    }).then( case => {
+        res.json(case);
+    })
+ })
+
+//@route        POST /api/cases
+//@desc         Create new case
+//@access       Public
+router.post("api/cases", (req, res) => {
+db.Case.create(res.body).then(newCase => {
+    res.status(201).json(newCase)
+})
+})
+
+//@route        PUT /api/cases/:id
+//@desc         Update cae
+//@access       Public
+router.put("api/cases/:id", (req,res) => {
+    db.Case.update({
+        where: {
+            id: req.body.id
+        }
+    }).then(case => {
+        res.json(case);
+    })
+})
+
+
+//@route        POST /api/cases
+//@desc         create new case
+//@access       Public
+router.delete("api/cases/:id", (req, res) => {
+    db.Case.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then( case => {
+        res.json(case);
+    })
+})
---
+++ routes/api/litigants.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const litigant = require("");
+
+//@route        GET all /api/litigants 
+//@desc         Ger all litigants
+//access        Public
+router.get("/api/litigants")
