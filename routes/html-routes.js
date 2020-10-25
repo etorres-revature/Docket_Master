a316f9017f9cfd57bb01e272fc2ebcae9924acfd
James Howard <jth2698@gmail.com>
2020-10-25 11:55:43 -0500

    build html-routes for handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,28 +4,37 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+
+    // Insert route to handlebars instead of /public/signup.html
+    res.render("signup.handlebars");
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+
+    // Insert route to handlebars instead of /public/login.html
+    res.render("login.handlebars");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/members", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
+
+    // Insert route to handlebars instead of /public/login.html
+    res.render("members.handlebars");
   });
 
 };
