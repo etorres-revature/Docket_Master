fa6d97fadb25b0674e29c821cae6a28f11b464fd
James Howard <jth2698@gmail.com>
2020-10-25 10:55:03 -0500

    add handlebars to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Requiring necessary npm packages
 var express = require("express");
+var exphbs = require("express-handlebars");
 var session = require("express-session");
+
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
@@ -13,6 +15,11 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Set Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -23,8 +30,8 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
