7b21ec4d9e16002de3828c109dfb38039f8ff797
James Howard <jth2698@gmail.com>
2020-10-25 20:19:31 -0500

    commit changes from main

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/html/html-routes.js")(app);
+require("./routes/api/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({ force: true }).then(function () {
