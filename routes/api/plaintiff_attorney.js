8a9e437ccd6f5b6dcff36c4b56b59d2b4c0b68ea
Eric D. Torres <etorresnotary@gmail.com>
2020-10-31 18:15:29 -0500

    add new attorney (defense and prosecutor) enabled

--- routes/api/plaintiff_attorney.js
+++ routes/api/plaintiff_attorney.js
@@ -7,60 +7,60 @@ const db = require("../../models");
 //@desc         Get all plaintiff attorneys
 //@access       Public
 router.get("/api/plaintiff_attorneys", (req, res) => {
-    db.PlaintiffAttorney.findAll({}).then((plaintAttys) => {
-        res.json(plaintAttys);
-    });
+  db.PlaintiffAttorney.findAll({}).then((plaintAttys) => {
+    res.json(plaintAttys);
+  });
 });
 
 //@route        GET /api/plaintiff_attorney/:id
 //@desc         Get plaintiff attorney
 //@access       Public
-router.get("/api/plaintiff_attorney/:id", (req, res) => {
-    const { id } = req.params;
-    db.PlaintiffAttorney.findOne({
-        where: {
-            id,
-        },
-    }).then((plaintAtty) => {
-        res.json(plaintAtty);
-    });
+router.get("/api/plaintiff_attorneys/:id", (req, res) => {
+  const { id } = req.params;
+  db.PlaintiffAttorney.findOne({
+    where: {
+      id,
+    },
+  }).then((plaintAtty) => {
+    res.json(plaintAtty);
+  });
 });
 
 //@route        POST /api/plaintiff_attorney
 //@desc         Create new plaintiff attorney
 //@access       Public
-router.post("api/plaintiff_attorney", (req, res) => {
-    db.PlaintiffAttorney.create(req.body).then((newPlaintAtty) => {
-        res.status(201).json(newPlaintAtty);
-    });
+router.post("/api/plaintiff_attorneys", (req, res) => {
+  db.PlaintiffAttorney.create(req.body).then((newPlaintAtty) => {
+    res.status(201).json(newPlaintAtty);
+  });
 });
 
 //@route        PUT /api/plaintiff_attorney/:id
 //@desc         Update plaintiff attorney
 //@access       Public
-router.put("api/plaintiff_attorney/:id", (req, res) => {
-    const { id } = req.params;
-    db.PlaintiffAttorney.update({
-        where: {
-            id,
-        },
-    }).then((plaintAtty) => {
-        res.json(plaintAtty);
-    });
+router.put("/api/plaintiff_attorneys/:id", (req, res) => {
+  const { id } = req.params;
+  db.PlaintiffAttorney.update({
+    where: {
+      id,
+    },
+  }).then((plaintAtty) => {
+    res.json(plaintAtty);
+  });
 });
 
 //@route        DELETE /api/plaintiff_attorney/:id
 //@desc         Delete plaintiff attorney
 //@access       Public
-router.delete("api/plaintiff_attorney/:id", (req, res) => {
-    const { id } = req.params;
-    db.PlaintiffAttorney.destroy({
-        where: {
-            id,
-        },
-    }).then((plaintAtty) => {
-        res.json(plaintAtty);
-    });
+router.delete("/api/plaintiff_attorneys/:id", (req, res) => {
+  const { id } = req.params;
+  db.PlaintiffAttorney.destroy({
+    where: {
+      id,
+    },
+  }).then((plaintAtty) => {
+    res.json(plaintAtty);
+  });
 });
 
 module.exports = router;
