ba0d91add3b96458cf1bf65e8c11c1fa39fe58af
trilambda122 <trilambda122>
2020-11-01 11:39:17 -0600

    resolved comflicts

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -7,43 +7,18 @@ const path = require("path");
 var isAuthenticated = require("../../config/middleware/isAuthenticated");
 const { ppid } = require("process");
 
+module.exports = function(app) {
 
-module.exports = function (app) {
-  app.get("/", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/docketmaster");
-    }
-    // Comment out pre-handlebars res.sendFile function
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    app.get("/", function(req, res) {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/docketmaster");
+        }
+        // Comment out pre-handlebars res.sendFile function
+        // res.sendFile(path.join(__dirname, "../public/signup.html"));
 
-    // Insert route to handlebars instead of /public/signup.html
-    res.render("signup.handlebars");
-  });
-
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/docketmaster");
-    }
-    // Comment out pre-handlebars res.sendFile function
-    // res.sendFile(path.join(__dirname, "../public/login.html"));
-
-    // Insert route to handlebars instead of /public/login.html
-    res.render("login.handlebars");
-  });
-
-  // Here we've add our isAuthenticated middleware to this route. // NEEDS TO BE ADDED BACK IN
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // Note that we are using async / await to populate data (divisions and cases) on the main docket master page
-
-  app.get("/docketmaster", async (req, res) => {
-    const divisions = await db.Division.findAll({});
-    const cases = await db.Case.findAll({});
-    res.render("docketmaster", {
-      divisions,
-      cases,
-
+        // Insert route to handlebars instead of /public/signup.html
+        res.render("signup.handlebars");
     });
 
     app.get("/login", function(req, res) {
@@ -54,41 +29,65 @@ module.exports = function (app) {
         // Comment out pre-handlebars res.sendFile function
         // res.sendFile(path.join(__dirname, "../public/login.html"));
 
-
-  app.get("/docketmaster/add", function (req, res) {
-    res.render("add.handlebars");
-  });
-
-
-  app.get("/docketmaster/admin/create", (req, res) => {
-    res.render("adminCreate.handlebars");
-  });
-};
-
-
-  app.get("/docketmaster/admin/views", async (req, res) => {
-    const plaintiffs = await db.Plaintiff.findAll({});
-    const defendants = await db.Defendant.findAll({});
-    const pAttys = await db.PlaintiffAttorney.findAll({});
-    const dAttys = await db.DefenseAttorney.findAll({});
-    const divisions = await db.Division.findAll({});
-    const types = await db.Type.findAll({});
-    res.render("adminView.handlebars", {
-      plaintiffs,
-      defendants,
-      pAttys,
-      dAttys,
-      divisions,
-      types,
+        // Insert route to handlebars instead of /public/login.html
+        res.render("login.handlebars");
     });
-  });
-};
-            
-app.get("/docketmaster/view", async(req, res) => {
 
-  const cases = await db.Case.findAll({
-      include: [db.Type, db.Division, db.Plaintiff, db.PlaintiffAttorney, db.Defendant, db.DefenseAttorney]
+    // Here we've add our isAuthenticated middleware to this route. // NEEDS TO BE ADDED BACK IN
+    // If a user who is not logged in tries to access this route they will be redirected to the signup page
+    // Note that we are using async / await to populate data (divisions and cases) on the main docket master page 
+
+    app.get("/docketmaster", async(req, res) => {
+        const divisions = await db.Division.findAll({});
+        const cases = await db.Case.findAll({
+            include: [db.Type, db.Division, db.Plaintiff, db.PlaintiffAttorney, db.Defendant, db.DefenseAttorney]
+        });
+        res.render("docketmaster", {
+            divisions,
+            cases
+        });
+    });
+
+
+    app.get("/docketmaster/add", function(req, res) {
+        res.render("add.handlebars");
+    });
+
+
+    app.get("/docketmaster/admin/create", (req, res) => {
+        res.render("adminCreate.handlebars");
+    });
+
+    app.get("/docketmaster/view", async(req, res) => {
+
+        const cases = await db.Case.findAll({
+            include: [db.Type, db.Division, db.Plaintiff, db.PlaintiffAttorney, db.Defendant, db.DefenseAttorney]
         });
 
         res.render("view.handlebars", { cases });
     });
+
+    app.get("/docketmaster/add", async(req, res) => {
+        const cases = await db.Case.findAll({
+            include: [db.Type, db.Division, db.Plaintiff, db.PlaintiffAttorney, db.Defendant, db.DefenseAttorney]
+        });
+        res.render("add.handlebars", { cases });
+    })
+
+    app.get("/docketmaster/admin/views", async(req, res) => {
+        const plaintiffs = await db.Plaintiff.findAll({});
+        const defendants = await db.Defendant.findAll({});
+        const pAttys = await db.PlaintiffAttorney.findAll({});
+        const dAttys = await db.DefenseAttorney.findAll({});
+        const divisions = await db.Division.findAll({});
+        const types = await db.Type.findAll({});
+        res.render("adminView.handlebars", {
+            plaintiffs,
+            defendants,
+            pAttys,
+            dAttys,
+            divisions,
+            types,
+        });
+    });
+};
