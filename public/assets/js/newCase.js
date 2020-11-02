4170bb8659e0391d9d6f3b3bde172fc209b99551
trilambda122 <trilambda122>
2020-11-01 21:18:27 -0600

    got autocomplete working on all fields

--- public/assets/js/newCase.js
+++ public/assets/js/newCase.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
     console.log('in the addCase js file');
 
-    // function to query the database for all defendants and filter the results as they are typed in the field
-    let defendantResults = async searchText => {
+    // DEFENDANTS:  function to query the database for all defendants and filter the results as they are typed in the field
+    let defendantResults = async function(searchText) {
         //use fetch to get the data back from the api
         const res = await fetch('/api/defendants');
         const defendants = await res.json();
@@ -25,24 +25,112 @@ $(document).ready(function() {
         }
 
         // console.log(matches);
-        outputHTML(matches);
+        outputHTML(matches, '#defendant-match-list');
+
 
     }
 
-    const outputHTML = matches => {
+    // PLAINTIFFS:  function to query the database for all defendants and filter the results as they are typed in the field
+    let plaintiffResults = async function(searchText) {
+        //use fetch to get the data back from the api
+        const res = await fetch('/api/plaintiffs');
+        const defendants = await res.json();
+        let fullNames = [];
+        defendants.forEach(item => {
+            let name = `${item.FName} ${item.LName}`;
+            fullNames.push(name);
+        });
+
+        let matches = fullNames.filter(fullname => {
+            const regex = new RegExp(`${searchText}`, 'gi');
+            return fullname.match(regex);
+        })
+        if (searchText.length === 0) {
+            matches = []
+            $('#plaintiff-match-list').html(html);
+        }
+
+        // console.log(matches);
+        outputHTML(matches, '#plaintiff-match-list');
+
+
+    }
+
+    // PLAINTIFFS ATTORNEYS:  function to query the database for all defendants and filter the results as they are typed in the field
+    let pAttorneyResults = async function(searchText) {
+        //use fetch to get the data back from the api
+        const res = await fetch('/api/plaintiff_attorneys');
+        const defendants = await res.json();
+        let fullNames = [];
+        defendants.forEach(item => {
+            let name = `${item.FName} ${item.LName}`;
+            fullNames.push(name);
+        });
+
+        let matches = fullNames.filter(fullname => {
+            const regex = new RegExp(`${searchText}`, 'gi');
+            return fullname.match(regex);
+        })
+        if (searchText.length === 0) {
+            matches = []
+            $('#pAttorney-match-list').html(html);
+        }
+
+        // console.log(matches);
+        outputHTML(matches, '#pAttorney-match-list');
+
+    }
+
+
+    // DEFENSE ATTORNEYS:  function to query the database for all defendants and filter the results as they are typed in the field
+    let dAttorneyResults = async function(searchText) {
+        //use fetch to get the data back from the api
+        const res = await fetch('/api/defense_attorneys');
+        const defendants = await res.json();
+        let fullNames = [];
+        defendants.forEach(item => {
+            let name = `${item.def_attorneyFName} ${item.def_attorneyLName}`;
+            fullNames.push(name);
+        });
+
+        let matches = fullNames.filter(fullname => {
+            const regex = new RegExp(`${searchText}`, 'gi');
+            return fullname.match(regex);
+        })
+        if (searchText.length === 0) {
+            matches = []
+            $('#dAttorney-match-list').html(html);
+        }
+
+        // console.log(matches);
+        outputHTML(matches, '#dAttorney-match-list');
+
+    }
+
+
+
+
+
+    const outputHTML = function(matches, htmlTarget) {
             if (matches.length > 0) {
                 const html = matches.map(
                     match => `<span><p>${match}</p></span>`
                 ).join('');
                 console.log(html);
-                $('#defendant-match-list').html(html);
+                $(htmlTarget).html(html);
 
             }
-
-
         }
         //   event listener for defendant name input field 
-    $('#update-defendant-fName').on('input', () => defendantResults($('#update-defendant-fName').val()));
+    $('#defendant-name').on('input', () => defendantResults($('#defendant-name').val()));
+    $('#plaintiff-name').on('input', () => plaintiffResults($('#plaintiff-name').val()));
+    $('#pAttorney-name').on('input', () => pAttorneyResults($('#pAttorney-name').val()));
+    $('#dAttorney-name').on('input', () => dAttorneyResults($('#dAttorney-name').val()));
+
+    $('#defendant-name').on('blur', () => { $('#defendant-match-list').html('') });
+    $('#plaintiff-name').on('blur', () => { $('#plaintiff-match-list').html('') });
+    $('#pAttorney-name').on('blur', () => { $('#pAttorney-match-list').html('') });
+    $('#dAttorney-name').on('blur', () => { $('#dAttorney-match-list').html('') });
 
 
 
